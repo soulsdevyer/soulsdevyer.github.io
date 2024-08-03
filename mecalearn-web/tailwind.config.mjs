217d5663fb3e08cd54092b9945ec5496106ec334
soulsdevyer <marcellayermain@gmail.com>
2024-08-03 15:29:56 -0500

    first commit - add tailwind

---
+++ mecalearn-web/tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "meca-mainBlue": "#0d1b2a",
+        "meca-secondaryBlue": "#1c3353",
+        "meca-midBlue": "#3d5f86",
+        "meca-lightBlue": "#6b8bb1",
+        "meca-lightestBlue": "#c5d3de",
+      },
+    },
+  },
+  plugins: [],
+};

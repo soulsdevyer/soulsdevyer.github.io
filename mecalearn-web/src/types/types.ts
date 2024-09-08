85bff7f04a5d9485275a342f127718e2f830c101
soulsdevyer <marcellayermain@gmail.com>
2024-09-07 19:29:40 -0500

    add types - add props - add subject navigation

---
+++ mecalearn-web/src/types/types.ts
@@ -0,0 +1,13 @@
+export interface NavItems {
+  label: string;
+  href: string;
+  iconPath1?: string;
+  iconPath2?: string;
+}
+
+export interface SubjectCards {
+  img: string;
+  title: string;
+  label: string;
+  href: string;
+}
